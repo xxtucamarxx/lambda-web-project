c7d74473ed40d434331b721c01b95e957f3e6a13
Tucamar <xxtucamarxx@hotmail.com>
2023-10-09 11:27:21 -0300

    adding first database querring

--- lambda_project/vsview/urls.py
+++ lambda_project/vsview/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from vsview.views import AboutPageView, HomePageView
+from vsview.views import AboutPageView, HomePageView, interactionPageView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('interaction/', interactionPageView, name='interaction'),
     path('about/', AboutPageView.as_view(), name='about')
 ]
--- lambda_project/vsview/views.py
+++ lambda_project/vsview/views.py
@@ -1,5 +1,15 @@
+import json
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import TemplateView
+from vsview.models import PocketInteract
 # Create your views here.
+
+def interactionPageView(request):
+    interaction = list(PocketInteract.objects.filter(id_pdb = '7KR1', id_pocket = 'pocket3').order_by('resnr'))
+    serialized_interaction = serializers.serialize('json', interaction)
+    output= json.dumps(json.loads(serialized_interaction), indent=4)
+    return HttpResponse(output, content_type='application/json')
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -7,4 +17,4 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
-    
+
