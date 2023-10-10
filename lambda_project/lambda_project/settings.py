ed4d4001688d945b03169c2f5112403cdbcf6566
xxtucamarxx <xxtucamarxx@gmail.com>
2023-10-10 12:27:17 -0300

    master database setup

--- lambda_project/lambda_project/settings.py
+++ lambda_project/lambda_project/settings.py
@@ -76,25 +76,36 @@ WSGI_APPLICATION = "lambda_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 # Connect to a server using the ssh keys. See the sshtunnel documentation for using password authentication
-ssh_tunnel = SSHTunnelForwarder(
-    ("150.165.131.12", 9022),
-    ssh_username="tucamar",
-    ssh_password="naoesqueca",
-    remote_bind_address=('192.168.0.1', 5433),
-)
-ssh_tunnel.start()
+# ssh_tunnel = SSHTunnelForwarder(
+#     ("150.165.131.12", 9022),
+#     ssh_username="tucamar",
+#     ssh_password="naoesqueca",
+#     remote_bind_address=('192.168.0.1', 5433),
+# )
+# ssh_tunnel.start()
+# DATABASES = {
+#     "default": {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'HOST': 'localhost',
+#         'PORT': ssh_tunnel.local_bind_port,
+#         'NAME': 'vs',
+#         'USER': 'web',
+#         'PASSWORD': 'web',
+#         'OPTIONS': {'options': '-c search_path=covid19'},
+#     }
+# }
+
 DATABASES = {
     "default": {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'HOST': 'localhost',
-        'PORT': ssh_tunnel.local_bind_port,
+        'HOST': '192.168.0.1',
+        'PORT': 5433,
         'NAME': 'vs',
         'USER': 'web',
         'PASSWORD': 'web',
         'OPTIONS': {'options': '-c search_path=covid19'},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
