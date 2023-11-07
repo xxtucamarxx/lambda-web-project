66568a20073537bcccd8da631e5680bf1fb7a490
Tucamar <xxtucamarxx@hotmail.com>
2023-11-07 15:00:25 -0300

    plip fertig

--- lambda_project/vsview/views.py
+++ lambda_project/vsview/views.py
@@ -1,27 +1,9 @@
 from django.views.generic import TemplateView
-<<<<<<< HEAD
-from vsview.forms import PlipListForm
-=======
->>>>>>> tuca
 from vsview.models import PocketInteract
 from django.shortcuts import render
 # Create your views here.
 
 def plipListPageView(request):
-<<<<<<< HEAD
-    if request.method == "POST":
-        form = PlipListForm(request.POST)
-        if form.is_valid():
-            id_pdb = form.cleaned_data['id_pdb']
-            id_pocket = form.cleaned_data['id_pocket']
-            interactions = PocketInteract.objects.filter(id_pdb=id_pdb, id_pocket=id_pocket)
-        else:
-            interactions = None
-    else:
-        form = PlipListForm()
-        interactions = None
-    context = {'form': form, 'interactions': interactions}
-=======
     # template_name = 'plip_list.html'
     interactions = None
     id_pdb = None
@@ -31,7 +13,6 @@ def plipListPageView(request):
         id_pocket = request.POST.get('pocket')
         interactions = PocketInteract.objects.filter(id_pdb=id_pdb, id_pocket=f'pocket{id_pocket}')
     context = {'interactions': interactions, 'id_pdb': id_pdb, 'id_pocket': id_pocket}
->>>>>>> tuca
 
     return render(request, 'plip_list.html', context)
 
