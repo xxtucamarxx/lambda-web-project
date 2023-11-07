f0f57ae2f8e724e1780ef43de7b7768fc3096ea5
Tucamar <xxtucamarxx@hotmail.com>
2023-11-07 14:39:15 -0300

    fertig

--- lambda_project/vsview/views.py
+++ lambda_project/vsview/views.py
@@ -1,27 +1,20 @@
-import json
 from django.views.generic import TemplateView
-from vsview.forms import PlipQueryForm
 from vsview.models import PocketInteract
 from django.shortcuts import render
 # Create your views here.
-#
 
 def plipListPageView(request):
+    # template_name = 'plip_list.html'
     interactions = None
     id_pdb = None
     id_pocket = None
     if request.method == "POST":
-        form = PlipQueryForm(request.POST)
-        if form.is_valid():
-            id_pdb = form.cleaned_data['id_pdb']
-            id_pocket = form.cleaned_data['id_pocket']
-            interactions = PocketInteract.objects.filter(id_pdb=id_pdb, id_pocket=id_pocket)
-    else:
-        form = PlipQueryForm()
-    context = {'form': form, 'interactions': interactions, 'id_pdb': id_pdb, 'id_pocket': id_pocket}
+        id_pdb = request.POST.get('pdb')
+        id_pocket = request.POST.get('pocket')
+        interactions = PocketInteract.objects.filter(id_pdb=id_pdb, id_pocket=f'pocket{id_pocket}')
+    context = {'interactions': interactions, 'id_pdb': id_pdb, 'id_pocket': id_pocket}
 
     return render(request, 'plip_list.html', context)
-    # template_name = 'interaction_query.html'
 
 
 class HomePageView(TemplateView):
