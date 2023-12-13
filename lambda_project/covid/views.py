e46b836a9f514eb207d758b1197ce7877925c556
xxtucamarxx <xxtucamarxx@gmail.com>
2023-12-13 15:17:38 -0300

    add post method

--- lambda_project/covid/views.py
+++ lambda_project/covid/views.py
@@ -1,25 +1,38 @@
-from django.core.serializers import serialize
-from django.db.models import Count
 from django.shortcuts import render
 
 from covid.models import VsRaw
 
 # Create your views here.
+def get_data_list(field: str, data):
+    return data.values(field).distinct(field)
 
 
 def covidListPageView(request):
     # template_name = 'covid_list.html'
-    interaction_complexes = None
-    farmacos = (
-        VsRaw.objects.exclude(nm_farmaco="profluis")
-        .values("id_pdb__nm_protein", "id_pdb", "nm_farmaco")
-        .distinct("id_pdb__nm_protein")
-    )
-    if request == "POST":
-        pass
+    data = VsRaw.objects.exclude(nm_farmaco="profluis", id_pdb="7K3G")
+
+    proteins_list = get_data_list("id_pdb__nm_protein", data)
+    farmacos_list = get_data_list("nm_farmaco", data)
+
     context = {
-        "complexes": interaction_complexes,
-        "farmacos": farmacos,
+        "proteins_list": proteins_list,
+        "farmacos_list": farmacos_list,
     }
 
+    if request.method == "POST":
+        protein = request.POST.get("protein")
+        # pdb = request.POST.get("pdb")
+        # farmaco = request.POST.get("farmaco")
+        data = VsRaw.objects.exclude(nm_farmaco="profluis", id_pdb="7K3G")
+        if protein :
+            data.filter(id_pdb__nm_protein=protein)
+        # if pdb: 
+        #     data.filter(id_pdb=pdb)
+        # if farmaco:
+        #     data.filter(nm_farmaco=farmaco)
+
+        context["complexes"] = data
+
+
     return render(request, "covid_list.html", context)
+
