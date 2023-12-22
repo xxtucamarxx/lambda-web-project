ff58ece7f6955b86fd402a456a441929ce7ecf9a
Tucamar <xxtucamarxx@hotmail.com>
2023-12-22 17:04:39 -0300

    covid

--- lambda_project/covid/views.py
+++ lambda_project/covid/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from covid.models import VsRaw
 
+
 # Create your views here.
 def get_data_list(field: str, data):
     return data.values(field).distinct(field)
@@ -9,30 +10,32 @@ def get_data_list(field: str, data):
 
 def covidListPageView(request):
     # template_name = 'covid_list.html'
-    data = VsRaw.objects.exclude(nm_farmaco="profluis", id_pdb="7K3G")
-
-    proteins_list = get_data_list("id_pdb__nm_protein", data)
-    farmacos_list = get_data_list("nm_farmaco", data)
+    # data = VsRaw.objects.exclude(nm_farmaco="profluis", id_pdb="7K3G")
+    #
+    # proteins_list = get_data_list("id_pdb__nm_protein", data)
+    # farmacos_list = get_data_list("nm_farmaco", data)
 
     context = {
-        "proteins_list": proteins_list,
-        "farmacos_list": farmacos_list,
+        # "proteins_list": proteins_list,
+        # "farmacos_list": farmacos_list,
     }
 
     if request.method == "POST":
         protein = request.POST.get("protein")
-        # pdb = request.POST.get("pdb")
-        # farmaco = request.POST.get("farmaco")
-        data = VsRaw.objects.exclude(nm_farmaco="profluis", id_pdb="7K3G")
-        if protein :
-            data.filter(id_pdb__nm_protein=protein)
-        # if pdb: 
-        #     data.filter(id_pdb=pdb)
-        # if farmaco:
-        #     data.filter(nm_farmaco=farmaco)
+        context["protein"] = protein
+        pdb = request.POST.get("pdb")
+        farmaco = request.POST.get("farmaco")
 
-        context["complexes"] = data
+        list_data = VsRaw.objects.exclude(nm_farmaco="profluis")
+        if protein:
+            list_data.filter(id_pdb__nm_protein=protein)
+        if pdb:
+            list_data.filter(id_pdb=pdb)
+            context["pdb"] = pdb
+        if farmaco:
+            list_data.filter(nm_farmaco=farmaco)
+            context["farmaco"] = farmaco
 
+        context["complexes"] = list_data
 
     return render(request, "covid_list.html", context)
-
--- lambda_project/vsview/views.py
+++ lambda_project/vsview/views.py
@@ -1,30 +1,43 @@
-from vsview.models import PocketInteract
-from django.views.generic import TemplateView
 from django.shortcuts import render
+from django.template import context
+from django.views.generic import TemplateView
+from vsview.models import PocketInteract
+
 # Create your views here.
+
 
 def plipListPageView(request):
     # template_name = 'plip_list.html'
-    interactions = None
-    id_pdb = None
-    id_pocket = None
-    residue_data = None
+    context = dict()
     if request.method == "POST":
-        id_pdb = request.POST.get('pdb')
-        id_pocket = request.POST.get('pocket')
-        interactions = PocketInteract.objects.filter(id_pdb=id_pdb, id_pocket=f'pocket{id_pocket}')
+        id_pdb = request.POST.get("pdb")
+        id_pocket = request.POST.get("pocket")
+        interactions = PocketInteract.objects.filter(
+            id_pdb=id_pdb, id_pocket=f"pocket{id_pocket}"
+        )
 
-        residue_data = ' '.join([''.join(map(str, list(e.values()))) for e in list(interactions.values('restype', 'resnr').distinct('resnr'))])
+        residue_data = " ".join(
+            [
+                "".join(map(str, list(e.values())))
+                for e in list(interactions.values("restype", "resnr").distinct("resnr"))
+            ]
+        )
 
-    context = {'interactions': interactions, 'id_pdb': id_pdb, 'id_pocket': id_pocket, 'residue': residue_data}
+        context.update(
+            {
+                "interactions": interactions,
+                "id_pdb": id_pdb,
+                "id_pocket": id_pocket,
+                "residue": residue_data,
+            }
+        )
 
-    return render(request, 'plip_list.html', context)
+    return render(request, "plip_list.html", context)
 
 
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
 
 
 class AboutPageView(TemplateView):
-    template_name = 'about.html'
-
+    template_name = "about.html"
